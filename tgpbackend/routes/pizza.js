82c8b5bc4130109cbe3cc5258d9c48a56f7918b2
Samuel Huayra <samuelhuayra@gmail.com>
2020-03-13 11:51:12 -0400

    pizza and topping CRUD APIs

--- tgpbackend/routes/pizza.js
+++ tgpbackend/routes/pizza.js
@@ -13,4 +13,45 @@ router.post(`/addPizza`, async (req, res, next) => {
         next(e);
     }
 });
+
+router.get(`/getPizzas`, async (req, res, next) => {
+    try {
+        const result = await PizzaModel.find().exec();
+        res.json(result);
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.get(`/getPizza/:_id`, async (req, res, next) => {
+    try {
+        if (!req.params._id) throw new Error('Id is blank');
+        const pizza = await PizzaModel.findById(req.params._id).exec();
+        res.json(pizza);
+    } catch (e) {
+        next(e);
+    }
+});
+router.put(`/editPizza/:_id`, async (req, res, next) => {
+    try {
+        if (!req.params._id) throw new Error('Id is blank');
+        if (!req.body.name) throw new Error('Name is blank');
+        await PizzaModel.findByIdAndUpdate(req.params._id, req.body).exec();
+        const pizza = await PizzaModel.findById(req.params._id).exec();
+        res.json(pizza);
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.delete(`/deletePizza/:_id`, async (req, res, next) => {
+    try {
+        if (!req.params._id) throw new Error('Id is blank');
+        var pizza = await PizzaModel.deleteOne({ _id: req.params._id }).exec();
+        res.json(pizza);
+    } catch (e) {
+        next(e);
+    }
+});
+
 module.exports = router;
--- tgpbackend/routes/topping.js
+++ tgpbackend/routes/topping.js
@@ -12,4 +12,46 @@ router.post(`/addTopping`, async (req, res, next) => {
         next(e);
     }
 });
+
+router.get(`/getToppings`, async (req, res, next) => {
+    try {
+        const result = await ToppingModel.find().exec();
+        res.json(result);
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.get(`/getTopping/:_id`, async (req, res, next) => {
+    try {
+        if (!req.params._id) throw new Error('Id is blank');
+        const topping = await ToppingModel.findById(req.params._id).exec();
+        res.json(topping);
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.put(`/editTopping/:_id`, async (req, res, next) => {
+    try {
+        if (!req.params._id) throw new Error('Id is blank');
+        if (!req.body.name) throw new Error('Name is blank');
+        await ToppingModel.findByIdAndUpdate(req.params._id, req.body).exec();
+        const topping = await ToppingModel.findById(req.params._id).exec();
+        res.json(topping);
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.delete(`/deleteTopping/:_id`, async (req, res, next) => {
+    try {
+        if (!req.params._id) throw new Error('Id is blank');
+        var topping = await ToppingModel.deleteOne({ _id: req.params._id }).exec();
+        res.json(topping);
+    } catch (e) {
+        next(e);
+    }
+});
+
 module.exports = router;
