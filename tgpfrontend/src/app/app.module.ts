879961a936a8e42e352634939083fc826e7a268c
Samuel Huayra <samuelhuayra@gmail.com>
2020-03-13 12:33:28 -0400

    frontend structure

--- tgpfrontend/src/app/app.module.ts
+++ tgpfrontend/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToppingComponent } from './topping/topping.component';
+
+import { MaterialModule } from "./core/material/material.module";
+import { HttpClientModule } from '@angular/common/http';
+import { NgHttpLoaderModule } from 'ng-http-loader';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToppingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    NgHttpLoaderModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- tgpfrontend/src/environments/environment.prod.ts
+++ tgpfrontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  endPoint: 'http://localhost:5000' 
 };
